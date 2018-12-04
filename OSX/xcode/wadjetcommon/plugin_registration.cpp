0e0da2c346e1fc69bcd9d3201e33b7019fad3eb5
vga256 <chris@tinyredstudio.com>
2018-12-04 11:00:39 -0700

    Added Ramp it Up and Mage's Initiation Projects
    - now compiles its own AGSteam plugin and copies to MI project location

--- OSX/xcode/wadjetcommon/plugin_registration.cpp
+++ OSX/xcode/wadjetcommon/plugin_registration.cpp
@@ -1,20 +1,19 @@
 //
 //  plugin_registration.cpp
-//  Shivah
+//  Mage's Initiation
 //
 //  Created by Nick Sonneveld on 26/06/2016.
-//
+//  modified by vga256 12/04/2018
 //
 
 #include "plugin_registration.hpp"
 
 #include "agsplugin.h"
 #include "plugin_engine.h"
-#include "agswadjetutil.h"
 
 typedef void (*t_engine_pre_init_callback)(void);
 extern void engine_set_pre_init_callback(t_engine_pre_init_callback callback);
-
+/*
 static void pl_register_builtin_plugin() {
 
   pl_register_builtin_plugin((InbuiltPluginDetails){
@@ -27,9 +26,9 @@ static void pl_register_builtin_plugin() {
   });
 
 }
-
+*/
 
 __attribute__((constructor))
 static void oninit_register_builtin_plugins() {
-  engine_set_pre_init_callback(&pl_register_builtin_plugin);
+  //engine_set_pre_init_callback(&pl_register_builtin_plugin);
 }
